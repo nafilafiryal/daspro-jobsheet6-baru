f8e190de4489bbdf23390f557dc1ae8519a4f4ff
nafilafiryal <nafilahfiryal05@gmail.com>
2024-10-06 21:27:53 +0700

    question number 2 experiment1

--- selection2Exp117.java
+++ selection2Exp117.java
@@ -2,15 +2,16 @@ import java.util.Scanner;
 
 public class selection2Exp117 {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
+        Scanner input17 = new Scanner(System.in);
         int year;
         System.out.print("Input year = ");
-        year = input.nextInt();
-        
-        if ((year%4) == 0) {
-        }if ((year%100) != 0) {
-            System.out.println("Leap year");
-        }else 
-        System.out.println("Not a Leap year");
-    }   
+        year = input17.nextInt();
+        if (year%4 == 0) {
+            if (year%100 == 0) {
+               if (year%400 == 0) {
+                System.out.println("leap year");
+               } else {System.out.println("Not a Leap year");}
+            } else {System.out.println("Leap year");}
+        } else {System.out.println("Not a leap year");}
+    }
 }
