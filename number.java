74df538c46cc3ec889c4bed5766883909a8b6707
nafilafiryal <nafilahfiryal05@gmail.com>
2024-10-09 20:57:14 +0700

    assignment flowchart week 6 number 1

---
+++ number.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class number {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int num1, num2, num3, max;
+
+        System.out.print("Input Number 1 = ");
+        num1 = input.nextInt();
+        System.out.print("Input Number 2 = ");
+        num2 = input.nextInt();
+        System.out.print("Input Number 3 = ");
+        num3 = input.nextInt();
+
+        max=num1;
+        if (num2>max) {
+            System.out.print("Max number is = " + num2);
+        } if (num3>max) {
+            System.out.print("Max number is = " + num3);
+            
+        } else if (num2==max) {
+            System.out.println("Max number is = " + num2);
+            
+        }
+    }
+}
